d8db45c0204089060f079d54fba14e3352fbbf80
qiuziz <qiuzhi.zhu@shuyun.com>
2017-06-13 17:03:28 +0800

    样式

--- js/index.js
+++ js/index.js
@@ -18,10 +18,17 @@ Random.prototype = {
 	},
 
 	_create: function() {
-		var len = this.dishesList.length;
-		var dish = this.dishesList[Math.floor(Math.random() * len)];
-		$('#createDishes').text(dish);
+		var len = this.dishesList.length,
+			a = Math.floor(Math.random() * len),
+			b = noRepeat(a, Math.floor(Math.random() * len)),
+			dish1 = this.dishesList[a],
+			dish2 = this.dishesList[b];
+		$('#createDishes').text('今晚菜谱：' + dish1 + ',' + dish2);
 	}
+}
+
+function noRepeat(a, b) {
+	return a === b ? noRepeat(a, Math.floor(Math.random() * len)) : b
 }
 
 new Random();
