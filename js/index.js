1054f3c97c12140c4a7023775a1fe8940c0bbbde
qiuziz <qiuzhi.zhu@shuyun.com>
2017-06-13 15:41:47 +0800

    create dish

--- js/index.js
+++ js/index.js
@@ -1,15 +1,26 @@
 function Random() {
+	this.dishesList = [];
 	this._init();
 }
 
 Random.prototype = {
 	_init: function() {
+		var that = this;
 		$.get({
 			url: './dishes.json',
 			success: function(res) {
-				console.log(res)
+				that.dishesList = res.name;
 			}
+		});
+		$('#createBtn').click(function() {
+			that._create();
 		})
+	},
+
+	_create: function() {
+		var len = this.dishesList.length;
+		var dish = this.dishesList[Math.floor(Math.random() * len)];
+		$('#createDishes').text(dish);
 	}
 }
 
