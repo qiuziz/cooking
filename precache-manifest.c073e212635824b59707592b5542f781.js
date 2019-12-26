51b6e598373d2ff8bb1f70b09f6653191d656972
qiuziz <zhu_anly@163.com>
2019-12-26 11:46:05 +0000

    Deploying to gh-pages from master 551057fce4886cef6e6979bd284b5fb6659033d4

---
+++ precache-manifest.c073e212635824b59707592b5542f781.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578dde2bb3a3e4b567efd8f73a5d9c13",
+    "url": "/cooking/index.html"
+  },
+  {
+    "revision": "c51ece28e3568afed8e0",
+    "url": "/cooking/static/css/main.af2e5e35.chunk.css"
+  },
+  {
+    "revision": "10644b4ef6400e79a37f",
+    "url": "/cooking/static/js/2.f34c21fb.chunk.js"
+  },
+  {
+    "revision": "c51ece28e3568afed8e0",
+    "url": "/cooking/static/js/main.5bb1e741.chunk.js"
+  },
+  {
+    "revision": "3007f07139f7fa3aa472",
+    "url": "/cooking/static/js/runtime~main.43a0683b.js"
+  },
+  {
+    "revision": "a6c1f7db16db79567f371da27ff5f3b6",
+    "url": "/cooking/static/media/main-cn-s.a6c1f7db.png"
+  },
+  {
+    "revision": "dacdc7c268de0ef233dbc99a2270062c",
+    "url": "/cooking/static/media/main-cn.dacdc7c2.png"
+  },
+  {
+    "revision": "8dcb50cd366e872e46a4a08ee14516ef",
+    "url": "/cooking/static/media/main-s.8dcb50cd.png"
+  },
+  {
+    "revision": "912fc448d07e352195201ef4cdcf7a50",
+    "url": "/cooking/static/media/main.912fc448.png"
+  }
+]);
